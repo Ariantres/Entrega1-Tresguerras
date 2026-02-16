13c68ed115859502146ea945e0c164c1c53d5e11
ArianCrowdar <ariantresguerras@gmail.com>
2026-02-16 15:53:39 -0300

    Primera entrega

---
+++ src/managers/CartManager.js
@@ -0,0 +1,60 @@
+import fs from "fs";
+
+export default class CartManager {
+  constructor(path) {
+    this.path = path;
+  }
+
+  async getCarts() {
+    try {
+      const data = await fs.promises.readFile(this.path, "utf-8");
+      return JSON.parse(data);
+    } catch {
+      return [];
+    }
+  }
+
+  async createCart() {
+    const carts = await this.getCarts();
+
+    const newId = carts.length > 0 ? carts[carts.length - 1].id + 1 : 1;
+
+    const newCart = {
+      id: newId,
+      products: [],
+    };
+
+    carts.push(newCart);
+
+    await fs.promises.writeFile(this.path, JSON.stringify(carts, null, 2));
+
+    return newCart;
+  }
+
+  async getCartById(id) {
+    const carts = await this.getCarts();
+    return carts.find((c) => c.id === Number(id));
+  }
+
+  async addProductToCart(cid, pid) {
+    const carts = await this.getCarts();
+    const cart = carts.find((c) => c.id === Number(cid));
+
+    if (!cart) return null;
+
+    const productInCart = cart.products.find((p) => p.product === Number(pid));
+
+    if (productInCart) {
+      productInCart.quantity++;
+    } else {
+      cart.products.push({
+        product: Number(pid),
+        quantity: 1,
+      });
+    }
+
+    await fs.promises.writeFile(this.path, JSON.stringify(carts, null, 2));
+
+    return cart;
+  }
+}
